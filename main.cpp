4c0f13e29ea628e44e4fdc1a5079f27ab7caeefd
Sergey Kopeliovich <burunduk30@gmail.com>
2019-05-24 23:17:23 +0300

    add uc prefix

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include "test.h"
 
-#include "trivialSolution.h"
-#include "firstApproach.h"
-#include "hyperLogLog.h"
-#include "hyperBitBit.h"
+#include "ucTrivialSolution.h"
+#include "ucFirstApproach.h"
+#include "ucHyperLogLog.h"
+#include "ucHyperBitBit.h"
 
 #include <set>
 
 int main() {
-	// test<UniqCounterTrivial>();
 	try {
 		const int N = 32 << 10;
-		// test<UniqCounterFirstApproach<N>>();
-		test<UniqCounterHyperLogLog<N>>();
+		// test<UniqCounterTrivial>();
+		test<UniqCounterFirstApproach<N>>();
+		// test<UniqCounterHyperLogLog<N>>();
 		// test<UniqCounterHyperBitBit>();
 	} catch (const char* msg) {
 		fprintf(stderr, "catched error: %s\n", msg);
